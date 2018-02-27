c02754f8a3df639118215f59ea1d668d9c6d97fd
Adam Varco <adamvarco@live.com>
2018-02-26 20:17:37 -0500

    Update TestCar.java

--- TestCar.java
+++ TestCar.java
@@ -4,7 +4,7 @@ public class TestCar {
 
 	public static void main(String[] args) {
 		Car car = new Car("work",21346.5,"Elentra",2013,"Hyundai","E5678E");
-		//car.PrintCar();
+		car.PrintCar();
 		car.go();
 		car.stop();
 
